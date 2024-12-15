e917df64d19e581d9e131c2e12f815305a628868
samudev <sambea2103@gmail.com>
2024-12-15 12:14:43 -0500

    feat: add astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx(), sitemap(), tailwind()],
+  output: 'server',
+  integrations: [mdx(), sitemap(), tailwind()],
+
+  env:{
+      schema:{
+          SHOW_BUY_BUTTON: envField.boolean({context:'server', access:'public'}),
+          SCORE_API_ENDPOINT: envField.string({context:'server', access:'public'}),
+      }
+  },
+
+  adapter: vercel()
 });
